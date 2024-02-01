9506600cc2ea090d34aca064bbf8f8941a184f7f
orgservice <146081596+orgservice@users.noreply.github.com>
2024-02-01 14:01:35 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -211,7 +211,7 @@ async def save_group_settings(group_id, key, value):
 def get_size(size):
     """Get size in readable format"""
 
-    units = ["MB", "GB", "TB", "PB", "EB"]
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
     size = float(size)
     i = 0
     while size >= 1024.0 and i < len(units):
