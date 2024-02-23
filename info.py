712da5ba84b67a6aefa2664366eede7ca750963f
orgservice <146081596+orgservice@users.noreply.github.com>
2024-02-23 12:48:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,8 +95,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://orgmovies-pre.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://orgmovies-pre.koyeb.app/".format(FQDN, PORT)
+URL = "https://orgmoviesbot-pre.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://orgmoviesbot-pre.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -111,9 +111,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://orgmovies-pre.koyeb.app/".format(FQDN)
+    URL = "https://orgmoviesbot-pre.koyeb.app/".format(FQDN)
 else:
-    URL = "https://orgmovies-pre.koyeb.app/".format(FQDN)
+    URL = "https://orgmoviesbot-pre.koyeb.app/".format(FQDN)
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
