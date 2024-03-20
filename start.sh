d597e1941299b2182c48187757dc6943b21da481
orgservice <146081596+orgservice@users.noreply.github.com>
2024-03-20 17:58:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/orgservice/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/orgservice/primefilterbot.git /primefilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /primefilterbot
 fi
-cd /VJ-FILTER-BOT 
+cd /primefilterbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
