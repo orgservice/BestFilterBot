abe699855cf25ee1a31e08b4620ad9d250fada70
orgservice <146081596+orgservice@users.noreply.github.com>
2024-03-27 06:53:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -169,12 +169,12 @@ I couldn't find anything related to that. Did you mean any one of these?</i></b>
     I_CUD_NT = """ɪ ᴄᴏᴜʟᴅɴ'ᴛ ꜰɪɴᴅ ᴀɴʏ ᴍᴏᴠɪᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ {}.
 ᴘʟᴇᴀꜱᴇ ᴄʜᴇᴄᴋ ᴛʜᴇ ꜱᴘᴇʟʟɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ ᴏʀ ɪᴍᴅʙ..."""
 
-    MVE_NT_FND = """ᴍᴏᴠɪᴇ ɴᴏᴛ ꜰᴏᴜɴᴅ ɪɴ ᴅᴀᴛᴀʙᴀꜱᴇ...
+    MVE_NT_FND = """<b>ᴍᴏᴠɪᴇ ɴᴏᴛ ꜰᴏᴜɴᴅ ɪɴ ᴅᴀᴛᴀʙᴀꜱᴇ...
     
-    Pleαѕe reqυeѕт ιɴ тнιѕ ɢroυp wiтн ғorмαт. мy мoderαтorѕ will υploαd iт ѕooɴ.
+ Pleαѕe reqυeѕт iɴ тнiѕ ɢroυp wiтн ғorмαт. мy мoderαтorѕ will υploαd iт ѕooɴ.
     
-    ✅ ᴄʜᴇᴄᴋ ғᴏʀᴍᴀᴛ 👇
-    https://t.me/c/2127187291/15"""
+ ✅ ᴄʜᴇᴄᴋ ғᴏʀᴍᴀᴛ 👇
+ https://t.me/c/2127187291/15</b>"""
 
     TOP_ALRT_MSG = """Cʜᴇᴄᴋɪɴɢ Fᴏʀ Mᴏᴠɪᴇ Iɴ Dᴀᴛᴀʙᴀsᴇ..."""
 
