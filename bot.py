a5a219fa28fb81e1d1235786e907a22749a9d0fd
orgservice <146081596+orgservice@users.noreply.github.com>
2024-05-12 19:41:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ async def Lazy_start():
     await Media.ensure_indexes()
     await Media2.ensure_indexes()
         #choose the right db by checking the free space
-        stats = await clientDB.command('dbStats')
+        stats = await clientDB.command('stats')
         #calculating the free db space from bytes to MB
         free_dbSize = round(512-((stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))), 2)
         if SECONDDB_URI and free_dbSize<10: #if the primary db have less than 10MB left, use second DB.
