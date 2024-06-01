d58300205728ac16346878818937f0c144967071
orgservice <146081596+orgservice@users.noreply.github.com>
2024-06-01 08:25:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://orgprime:orgprime@imaxrobot.3gww5lw.mongodb.net/?retryWrites=true&w=majority")
-SECONDDB_URI = environ.get('SECONDDB_URI', "")
+SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://orgprime:orgprime@imax2nd.peplvim.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Collection')
 
