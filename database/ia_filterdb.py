7b527eb9b84840bf4bc802c31e6c1f6fd6491567
orgservice <146081596+orgservice@users.noreply.github.com>
2024-06-21 21:57:50 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -164,10 +164,10 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
         cursorSkipper = (next_offset-(await Media2.count_documents(filter)))
         cursor.skip(cursorSkipper if cursorSkipper>=0 else 0).limit(max_results-len(fileList2))
         fileList1 = await cursor.to_list(length=(max_results-len(fileList2)))
-        files = fileList2+fileList1
+        files = fileList1+fileList2
         next_offset = next_offset + len(fileList1)
     else:
-        files = fileList2
+        files = fileList1
         next_offset = offset + max_results
     if next_offset >= total_results:
         next_offset = ''
@@ -206,7 +206,7 @@ async def get_bad_files(query, file_type=None, filter=False):
     cursor.sort('$natural', -1)
     cursor2.sort('$natural', -1)
     # Get list of files
-    files = ((await cursor.to_list(length=(await Media.count_documents(filter))))+(await cursor2.to_list(length=(await Media2.count_documents(filter)))))
+    files = ((await cursor2.to_list(length=(await Media2.count_documents(filter))))+(await cursor.to_list(length=(await Media.count_documents(filter)))))
 
     #calculate total results
     total_results = len(files)
