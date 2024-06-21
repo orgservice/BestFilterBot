f81bf391cc99af9ff1cd7ab4a4ccb976c46feeb8
orgservice <146081596+orgservice@users.noreply.github.com>
2024-06-21 20:38:48 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -1,4 +1,3 @@
-
 import logging
 from struct import pack
 import re
@@ -159,12 +158,12 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     # Slice files according to offset and max results
     cursor2.skip(offset).limit(max_results)
     # Get list of files
-    fileList2 = await cursor2.to_list(length=max_results)
+    fileList2 = await cursor.to_list(length=max_results)
     if len(fileList2)<max_results:
         next_offset = offset+len(fileList2)
         cursorSkipper = (next_offset-(await Media2.count_documents(filter)))
         cursor.skip(cursorSkipper if cursorSkipper>=0 else 0).limit(max_results-len(fileList2))
-        fileList1 = await cursor.to_list(length=(max_results-len(fileList2)))
+        fileList1 = await cursor2.to_list(length=(max_results-len(fileList2)))
         files = fileList2+fileList1
         next_offset = next_offset + len(fileList1)
     else:
