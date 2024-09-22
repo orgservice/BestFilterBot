e007db8cdb6fb711e62bb72efa670dd6466d4615
orgservice <146081596+orgservice@users.noreply.github.com>
2024-09-22 13:42:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,13 +97,13 @@ NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME', "primefilterbot")
+    APP_NAME = environ.get('APP_NAME', "")
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "http://147.79.68.161:8086/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "http://147.79.68.161:8086/".format(FQDN, PORT)
+URL = "".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
