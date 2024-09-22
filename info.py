231fd10f54289abb1a9069eeb144b45006067800
orgservice <146081596+orgservice@users.noreply.github.com>
2024-09-22 14:02:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,9 +100,9 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME', "")
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '147.79.68.161:8086'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "http://147.79.68.161:8086/".format(FQDN) if ON_HEROKU or NO_PORT else \
+URL = "".format(FQDN) if ON_HEROKU or NO_PORT else \
     "http://147.79.68.161:8086/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
@@ -116,7 +116,7 @@ if 'DYNO' in environ:
 
 else:
     ON_HEROKU = False
-HAS_SSL=bool(getenv('HAS_SSL',False))
+HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
     URL = "http://147.79.68.161:8086/".format(FQDN)
 else:
