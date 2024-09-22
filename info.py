7dd188b07f5d10d145d70372663a1cf871517a2d
orgservice <146081596+orgservice@users.noreply.github.com>
2024-09-22 14:22:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,8 +101,8 @@ if 'DYNO' in environ:
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "http://147.79.68.161/".format(FQDN) if ON_HEROKU or NO_PORT else \
+FQDN = "http://147.79.68.161:8086/"
+URL = "http://147.79.68.161:8086/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "http://147.79.68.161:8086/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
@@ -110,17 +110,11 @@ SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
 MULTI_CLIENT = False
 name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
 
-else:
-    ON_HEROKU = False
-HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "http://147.79.68.161/".format(FQDN)
+    URL = "http://147.79.68.161:8086/".format(FQDN)
 else:
-    URL = "http://147.79.68.161/".format(FQDN)
+    URL = "http://147.79.68.161:8086/".format(FQDN)
     
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
