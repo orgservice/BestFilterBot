92ec07878e191f04c1df283241dd21a0d052384f
orgservice <146081596+orgservice@users.noreply.github.com>
2024-09-29 13:22:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,13 +95,13 @@ NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME', "imaxfiltersbot")
+    APP_NAME = environ.get('APP_NAME', "imaxfilterbot")
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN, PORT)
+URL = "https://imaxfilterbot-8dd8b383d80d.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://imaxfilterbot-8dd8b383d80d.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -116,9 +116,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN)
+    URL = "https://imaxfilterbot-8dd8b383d80d.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN)
+    URL = "https://imaxfilterbot-8dd8b383d80d.herokuapp.com/".format(FQDN)
     
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
