b429003ec099f2fe044dbef7e7a628e143ab3827
orgservice <146081596+orgservice@users.noreply.github.com>
2024-09-29 13:19:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-PORT = environ.get("PORT", "3000")
+PORT = environ.get("PORT", "8080")
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviespremium')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/orgprime')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/orgpremium/7')
@@ -95,13 +95,13 @@ NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME')
+    APP_NAME = environ.get('APP_NAME', "imaxfiltersbot")
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '147.79.68.161'))
-FQDN = str(getenv('FQDN', '147.79.68.161'))
-URL = "http://{}:3000/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "http://{}:3000/".format(FQDN, PORT)
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+URL = "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -116,9 +116,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "http://{}:3000/".format(FQDN)
+    URL = "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN)
 else:
-    URL = "http://{}:3000/".format(FQDN)
+    URL = "https://imaxfilterbot-99edf979b3f5.herokuapp.com/".format(FQDN)
     
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
