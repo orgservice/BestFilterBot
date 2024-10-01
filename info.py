3ce624669cb66e638b3f63bc1fd3db4f6002f026
orgservice <146081596+orgservice@users.noreply.github.com>
2024-10-01 09:42:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,25 +103,13 @@ else:
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '194.238.18.26'))
 FQDN = str(getenv('FQDN', '194.238.18.26:3000')) if not ON_HEROKU or getenv('FQDN', '194.238.18.26:3000') else APP_NAME+'.herokuapp.com'
 URL = "http://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "http://{}/".format(FQDN, PORT)
+    "http://{}/".format(FQDN)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
 MULTI_CLIENT = False
 name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
-
-else:
-    ON_HEROKU = False
-HAS_SSL=bool(getenv('HAS_SSL',True))
-
-if HAS_SSL:
-    URL = "http://{}/".format(FQDN)
-else:
-    URL = "http://{}/".format(FQDN)
     
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
