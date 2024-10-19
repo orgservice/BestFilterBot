d39e20c98132a1c4ee38648a8fbe58a7339db31a
orgservice <146081596+orgservice@users.noreply.github.com>
2024-10-19 17:41:34 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import logging
 from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
@@ -115,7 +111,3 @@ def get_reply_markup(query):
         ]
         ]
     return InlineKeyboardMarkup(buttons)
-
-
-
-
